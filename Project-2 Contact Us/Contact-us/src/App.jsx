4edb76cfe2830baaef446e75e83934dc067c0850
JYOTI MARKUN <jyotimarkun1997@gmail.com>
2024-10-10 17:05:03 +0530

    add css moduke

--- Project-2 Contact Us/Contact-us/src/App.jsx
+++ Project-2 Contact Us/Contact-us/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './Component/Header'
-import Navbar from './Component/Navbar'
+import Navbar from './Component/Navigation/Navbar'
 
 
 function App() {
--- Project-2 Contact Us/Contact-us/src/Component/Navigation/Navbar.jsx
+++ Project-2 Contact Us/Contact-us/src/Component/Navigation/Navbar.jsx
@@ -1,12 +1,16 @@
-import logo from "../Images/brand_logo.svg";
+import styles from "./Navigation.module.css";
+
+
+
 export default function Navbar(){
+    console.log(styles)
     return (
         <>
        <nav>
-      <div className="logo">
-      <img src={logo} alt="logo-image" />
+      <div >
+      <img src="../Images/brand_logo.svg" alt="logo-image" />
       </div>
-       <ul className="menu-item">
+       <ul className={`${styles.menu-item}`}>
        <li>Home</li>
         <li>About</li>
         <li>Contact Us</li>
